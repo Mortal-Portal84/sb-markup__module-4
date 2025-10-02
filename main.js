147c2eb441119b2cca146e913609f139c9456ee1
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-10-02 12:32:04 +0300

    optimize JS and styles for buttons

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 const contentViewButtons = document.querySelectorAll('.page-content__toggle')
 const pageContentView = document.querySelector('.page-content__main')
 
-contentViewButtons[0].addEventListener('click', (e) => {
-  if(contentViewButtons[1].classList.contains('page-content__toggle--current')) {
-    contentViewButtons[1].classList.remove('page-content__toggle--current')
-    contentViewButtons[0].classList.add('page-content__toggle--current')
-  }
+contentViewButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    contentViewButtons.forEach(btn => btn.classList.remove('page-content__toggle--current'))
+    button.classList.add('page-content__toggle--current')
 
-  pageContentView.classList.add('page-content__main--horizontal')
-})
-
-contentViewButtons[1].addEventListener('click', (e) => {
-  if(contentViewButtons[0].classList.contains('page-content__toggle--current')) {
-    contentViewButtons[0].classList.remove('page-content__toggle--current')
-    contentViewButtons[1].classList.add('page-content__toggle--current')
-  }
-
-  pageContentView.classList.remove('page-content__main--horizontal')
+    pageContentView.classList.toggle(
+      'page-content__main--horizontal',
+      index === 0
+    )
+  })
 })
